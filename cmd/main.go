ebdfc03a102eed875d1fdd5729a9beef5ab215f7
jesintharnold <jesintharnold@gmail.com>
2024-10-29 23:56:06 +0530

    mDNS discovery & Beacon completed

--- cmd/main.go
+++ cmd/main.go
@@ -14,20 +14,20 @@ func main() {
         log.Fatal("Usage: program [E/D] (E for Emitter, D for Discoverer)")
     }
 
+    stopchan:=make(chan os.Signal,1)
+	signal.Notify(stopchan,os.Interrupt,syscall.SIGTERM)
+
     role := strings.ToUpper(os.Args[1])
     switch role {
     case "E":
         log.Println("Starting in Emitter mode...")
-        go discovery.EmitPeerDiscovery()
+        go discovery.EmitPeerDiscovery(stopchan)
     case "D":
         log.Println("Starting in Discoverer mode...")
-        go discovery.DiscoverPeers()
+        go discovery.DiscoverPeers(stopchan)
     default:
         log.Fatal("Invalid role. Use 'E' for Emitter or 'D' for Discoverer")
     }
-
-	stopchan:=make(chan os.Signal,1)
-	signal.Notify(stopchan,os.Interrupt,syscall.SIGTERM)
     <-stopchan
-    log.Println("Shutting Down")
+    log.Println("Shutting down goshare application.")
 }
--- internal/discovery/discovery.go
+++ internal/discovery/discovery.go
@@ -4,14 +4,10 @@ import (
 	"context"
 	"log"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
 	"github.com/grandcat/zeroconf"
 )
 
-func EmitPeerDiscovery() {
+func EmitPeerDiscovery(shutdown <- chan os.Signal) {
 	const port = 42424
 	const serviceName = "_filetransfer._tcp"
 	hostname, err := os.Hostname()
@@ -25,37 +21,31 @@ func EmitPeerDiscovery() {
 		panic(err)
 	}
 	defer server.Shutdown()
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-	select {
-	case <-sig:
-		// Exit by user
-	case <-time.After(time.Second * 120):
-		// Exit by timeout
-	}
+	<- shutdown
 }
 
-func DiscoverPeers() {
+func DiscoverPeers(shutdown <- chan os.Signal) {
 	const serviceName = "_filetransfer._tcp"
 	resolver, err := zeroconf.NewResolver(nil)
 	if err != nil {
 		log.Fatalln("Failed to initialize resolver:", err.Error())
 	}
-
 	entries := make(chan *zeroconf.ServiceEntry)
-	go func(results <-chan *zeroconf.ServiceEntry) {
-		for entry := range results {
-			log.Println(entry)
+	go func() {
+		for entry := range entries {
+			// log.Println(entry)
+			log.Printf("Discovered peer: %s - %s\n", entry.HostName, entry.AddrIPv4)
 		}
-		log.Println("No more entries.")
-	}(entries)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*1)
-	defer cancel()
-	err = resolver.Browse(ctx, serviceName, "local.", entries)
+	}()
+	
+	disparentctx,disparentcancel:=context.WithCancel(context.Background())
+	defer disparentcancel()
+	log.Println("Alive Discovery")
+	err = resolver.Browse(disparentctx, serviceName, "local.", entries)
 	if err != nil {
 		log.Fatalln("Failed to browse:", err.Error())
 	}
-
-	<-ctx.Done()
+	<- shutdown
+	log.Println("Shutting down peer discovery.")
+	close(entries)
 }
