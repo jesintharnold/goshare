7f1645d8497ce976a550bc4f97cde1a8597ffd4f
jesintharnold <jesintharnold@gmail.com>
2024-11-22 22:41:35 +0530

    1. Added parent and client generation block| 2. Tested in local setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,57 +1,59 @@
 package main
 
-import (
-	"context"
-	"goshare/internal/discovery"
-	"goshare/internal/transfer"
-	"log"
-	"os"
-	"os/signal"
-	"strings"
-	"syscall"
-)
+import "goshare/internal/fileshare"
 
-func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("Usage: program [E/D] (E for Emitter, D for Discoverer)")
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	stopchan := make(chan os.Signal, 1)
-	signal.Notify(stopchan, os.Interrupt, syscall.SIGTERM)
-	role := strings.ToUpper(os.Args[1])
-	switch role {
-	case "E":
-		log.Println("Starting in Emitter mode...")
-		go discovery.EmitPeerDiscovery(ctx)
-		transferService := transfer.TransferService{
-			Ctx: ctx,
-		}
-		go transferService.ListenToPeer()
+// import (
+// 	"context"
+// 	"goshare/internal/discovery"
+// 	"goshare/internal/transfer"
+// 	"log"
+// 	"os"
+// 	"os/signal"
+// 	"strings"
+// 	"syscall"
+// )
 
-	case "D":
-		log.Println("Starting in Discoverer mode...")
-		go discovery.DiscoverPeers(ctx)
-		peerInfo := discovery.PeerInfo{
-			ID:        "jesi",
-			Name:      "craxy-jesi",
-			IPAddress: "192.168.0.105",
-			Port:      42424,
-		}
+// func main() {
+// 	if len(os.Args) < 2 {
+// 		log.Fatal("Usage: program [E/D] (E for Emitter, D for Discoverer)")
+// 	}
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	defer cancel()
+// 	stopchan := make(chan os.Signal, 1)
+// 	signal.Notify(stopchan, os.Interrupt, syscall.SIGTERM)
+// 	role := strings.ToUpper(os.Args[1])
+// 	switch role {
+// 	case "E":
+// 		log.Println("Starting in Emitter mode...")
+// 		go discovery.EmitPeerDiscovery(ctx)
+// 		transferService := transfer.TransferService{
+// 			Ctx: ctx,
+// 		}
+// 		go transferService.ListenToPeer()
 
-		test_transfer := transfer.TransferService{
-			Peerinfo: peerInfo,
-			Ctx:      ctx,
-		}
-		test_transfer.ConnectToPeer()
+// 	case "D":
+// 		log.Println("Starting in Discoverer mode...")
+// 		go discovery.DiscoverPeers(ctx)
+// 		peerInfo := discovery.PeerInfo{
+// 			ID:        "jesi",
+// 			Name:      "craxy-jesi",
+// 			IPAddress: "192.168.0.105",
+// 			Port:      42424,
+// 		}
 
-	default:
-		log.Fatal("Invalid role. Use 'E' for Emitter or 'D' for Discoverer")
-	}
-	<-stopchan
-	cancel()
-	log.Println("Shutting down goshare application.")
-}
+// 		test_transfer := transfer.TransferService{
+// 			Peerinfo: peerInfo,
+// 			Ctx:      ctx,
+// 		}
+// 		test_transfer.ConnectToPeer()
+
+// 	default:
+// 		log.Fatal("Invalid role. Use 'E' for Emitter or 'D' for Discoverer")
+// 	}
+// 	<-stopchan
+// 	cancel()
+// 	log.Println("Shutting down goshare application.")
+// }
 
 // package main
 
@@ -89,3 +91,8 @@ func main() {
 // 		fmt.Printf("Word: %s, Index: %d\n", word, index)
 // 	}
 // }
+
+func main() {
+	//fileshare.GenerateClientCerts()
+	//fileshare.GenerateParentCerts()
+}
---
+++ internal/fileshare/certifcates.go
@@ -0,0 +1,165 @@
+package fileshare
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"log"
+	"math/big"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func GenerateParentCerts() {
+	dirName := "PARENT"
+	err := os.Mkdir(dirName, 0755)
+	if err != nil {
+		if os.IsExist(err) {
+			log.Printf("Directory %s already exists.\n", dirName)
+		} else {
+			log.Fatalf("Failed to create directory: %v", err)
+		}
+	} else {
+		log.Printf("Directory %s created successfully.\n", dirName)
+	}
+
+	randnum, _ := rand.Int(rand.Reader, big.NewInt(1).Lsh(big.NewInt(1), 64))
+	certmetadata := &x509.Certificate{
+		SerialNumber: randnum,
+		Subject: pkix.Name{
+			Organization: []string{"goshare", "goshareCLI"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(180 * 24 * time.Hour),
+		IsCA:                  true,
+		BasicConstraintsValid: true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+	}
+
+	parentCA, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatalf("Failed to generate CA private key: %v", err)
+	}
+
+	certBytes, err := x509.CreateCertificate(rand.Reader, certmetadata, certmetadata, &parentCA.PublicKey, parentCA)
+	if err != nil {
+		log.Fatalf("Failed to create Parent CA certificate: %v", err)
+	}
+
+	caCertFile, err := os.Create(filepath.Join(dirName, "parentCA.crt"))
+	if err != nil {
+		log.Fatalf("Failed to create PARENT ca.crt: %v", err)
+	}
+	defer caCertFile.Close()
+
+	caKeyFile, err := os.Create(filepath.Join(dirName, "parent.key"))
+	if err != nil {
+		log.Fatalf("Failed to create parent.key: %v", err)
+	}
+	defer caKeyFile.Close()
+
+	err = pem.Encode(caCertFile, &pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
+	if err != nil {
+		log.Fatalf("Failed to write Parent CA certificate to file: %v", err)
+	}
+	log.Println("CA certificate saved to parentCA.crt")
+
+	err = pem.Encode(caKeyFile, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(parentCA),
+	})
+	if err != nil {
+		log.Fatalf("Failed to write key to file: %v", err)
+	}
+	log.Println("Key saved to parent-ca.key")
+}
+
+func GenerateClientCerts() {
+	clientcert, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatalf("Failed to create client certificate :%v", err)
+	}
+	randnum, _ := rand.Int(rand.Reader, big.NewInt(1).Lsh(big.NewInt(1), 64))
+	certmetadata := &x509.Certificate{
+		SerialNumber: randnum,
+		Subject: pkix.Name{
+			Organization: []string{"goshare", "goshareCLI"},
+		},
+		NotBefore:   time.Now(),
+		NotAfter:    time.Now().Add(180 * 24 * time.Hour),
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+	}
+	dirparentName := "PARENT"
+	dirName := "CERT"
+
+	//load existing parent key now
+	parentcert, err := tls.LoadX509KeyPair(filepath.Join(dirparentName, "parentCA.crt"), filepath.Join(dirparentName, "parent.key"))
+	if err != nil {
+		log.Fatalf("Failed to load CA certificate: %v", err)
+	}
+
+	if err != nil {
+		log.Fatalf("Failed to parse Parent CA private key: %v", err)
+	}
+
+	clientCert, err := x509.CreateCertificate(rand.Reader, certmetadata, certmetadata, &clientcert.PublicKey, parentcert.PrivateKey)
+	if err != nil {
+		log.Fatalf("Failed to create Client CA certificate: %v", err)
+	}
+
+	//we are saving the keys to client
+	err = os.Mkdir("CERT", 0755)
+	if err != nil {
+		if os.IsExist(err) {
+			log.Printf("Directory %s already exists.\n", dirName)
+		} else {
+			log.Fatalf("Failed to create directory: %v", err)
+		}
+	} else {
+		log.Printf("Directory %s created successfully.\n", dirName)
+	}
+
+	clientcrt, err := os.Create(filepath.Join(dirName, "client.crt"))
+	if err != nil {
+		log.Fatalf("Error while creating client crt ,%v", err)
+	}
+	defer clientcrt.Close()
+
+	clientkey, err := os.Create(filepath.Join(dirName, "client.key"))
+	if err != nil {
+		log.Fatalf("Error while creating client key ,%v", err)
+	}
+	defer clientkey.Close()
+
+	//Now encode the pem key to CA.crt & ca.key
+	err = pem.Encode(clientcrt, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: clientCert,
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to write to Client.crt")
+	}
+	log.Println("clientCA.crt is created")
+
+	parentPrivateKey, ok := parentcert.PrivateKey.(*rsa.PrivateKey)
+	if !ok {
+		log.Fatalf("Failed to convert parent private key to *rsa.PrivateKey")
+	}
+
+	err = pem.Encode(clientkey, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(parentPrivateKey),
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to write to Client.key")
+	}
+	log.Println("client.key is created")
+}
